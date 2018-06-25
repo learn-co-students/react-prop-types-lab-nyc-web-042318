afd9d540c561bafe2163909fd1ac64cdbf3ddce3
Derek Chen <derekgc@icloud.com>
2018-06-25 18:08:06 -0400

    not done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,28 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component{
+  static defaultProps ={
+    hasWatermark: false
+  }
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    producer: PropTypes.string,
+    hasWatermark: PropTypes.bool,
+    color: PropTypes.oneOf(['white', 'eggshell-white','salmon']).isRequired,
+    weight: PropTypes.number.isRequired
+  }
+
+
+  
+
+  render(){
+    return (
+      <p>{this.props.name}</p>
+    )
+  }
+}
+
+export default Product;
